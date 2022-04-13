31b540f1479f38db722c7b17db4d4936f855eba5
Saulo Rodrigues Maia <saulormaia@hotmail.com>
2022-04-13 16:56:15 -0300

    Mensagem de vencedor e perdedor

--- scripit.js
+++ scripit.js
@@ -1,18 +1,50 @@
 const cellElements = document.querySelectorAll("[data-cell]");
 const board = document.querySelector("[data-board]");
+const winningMessageTextElement = document.querySelector("[data-winning-message-text]");
+const winningMessage = document.querySelector("[data-winning-message-text]"); 
 
 let isCircleTurn;
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
 const startGame = () => {
     for (const cell of cellElements) {
         cell.addEventListener("click", handleClick, {once:true});
 
-        isCircleTurn = false;
+        isCircleTurn;
 
         board.classList.add("x");
     };
-    
-}
+};
+
+const endGame = (isDraw) => {
+    if (isDraw) {
+        winningMessageTextElement.innerText = "Empate!!!"
+    } else { 
+        winningMessageTextElement.innerText = isCircleTurn 
+        ? "O Venceu"
+        : "X Venceu";
+    }
+};
+
+
+// Verificar por vitória
+const checkForWin = (currentPlayer) => {
+    return winningCombinations.some((combination) => {
+        return combination.every((index) => {
+            return cellElements[index].classList.contains(currentPlayer);
+        }); 
+    });
+};
 
 const placeMark = (cell, classToAdd) => {
     cell.classList.add(classToAdd);
@@ -40,6 +72,10 @@ const handleClick = (e) => {
     placeMark(cell, classToAdd);
 
     // Verificar por vitória
+    const isWin = checkForWin(classToAdd);
+    if (isWin) {
+        endGame(true);
+    }
 
     // Verificar por empate
 
